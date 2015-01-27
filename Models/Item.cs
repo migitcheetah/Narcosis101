bffe009f82a611471883c1a5921c9f2519179395
migitcheetah <migitcheetah@gmail.com>
2015-01-27 11:36:21 -0800

    Narcosis101
    Narcosis101 website

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Narcosis101.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Camera { get; set; }
+        public string Flash { get; set; }
+        public string Lense { get; set; }
+
+        public virtual Camera Camera { get; set; }
+        public virtual Lense Lense { get; set; }
+        public virtual Flash Flash { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Narcosis101.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
